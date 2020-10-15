7e820922adb2916f036dd49c20882334c4ee8756
ImaneElAssari <imaneelassari@hotmail.com>
2020-10-15 02:32:00 +0200

    commit charge last2

---
+++ front-end/src/Services/ChargeService.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+const Charge_URL="http://localhost:8080/charges";
+class ChargeService{
+    getChargeById(chargeId){
+        return axios.get(Charge_URL+'/'+chargeId);
+    }
+    updateCharge(charge,chargeId){
+        return axios.put("http://localhost:8080/update-charges/"+chargeId,charge);
+    }
+
+    deleteCharge(chargeId){
+        return axios.delete("http://localhost:8080/delete-charge/"+chargeId)
+    }
+}
+
+export default new ChargeService();
